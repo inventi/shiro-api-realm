db61a19abf4112d681a637a0ecee31cc1ff46b37
Žygimantas <zygimantas@inventi.lt>
2017-09-26 10:37:07 +0300

    feat(shiro): add unauthorized exception handling

---
+++ src/main/java/io/inventi/shiro/api/realm/service/AuthorizationExceptionHandler.java
@@ -0,0 +1,17 @@
+package io.inventi.shiro.api.realm.service;
+
+import org.apache.shiro.authz.AuthorizationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AuthorizationExceptionHandler {
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<String> authorizationErrorHandler(AuthorizationException e) throws Exception {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+}
