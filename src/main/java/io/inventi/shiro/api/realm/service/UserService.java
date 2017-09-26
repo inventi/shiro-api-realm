dd1fcf5f28c2d55dffe62a79814342f8b9519dff
Žygimantas <zygimantas@inventi.lt>
2017-09-26 11:18:21 +0300

    fix(api): change rest client ex handling by specific status codes

--- src/main/java/io/inventi/shiro/api/realm/service/UserService.java
+++ src/main/java/io/inventi/shiro/api/realm/service/UserService.java
@@ -2,10 +2,8 @@ package io.inventi.shiro.api.realm.service;
 
 import io.inventi.shiro.api.realm.domain.User;
 
-import java.util.Optional;
-
 public interface UserService {
 
-    Optional<User> findUser(String userId);
+    User getUser(String userId);
 
 }
--- src/main/java/io/inventi/shiro/api/realm/service/UserServiceImpl.java
+++ src/main/java/io/inventi/shiro/api/realm/service/UserServiceImpl.java
@@ -6,7 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.client.RestClientException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -26,35 +27,37 @@ public class UserServiceImpl implements UserService {
                 .build();
     }
 
-    public Optional<User> findUser(String userId) {
-        Optional<User> user = getUser(userId);
-        if (user.isPresent()) {
-            return user;
-        }
-        return createUser(userId);
+    public User getUser(String userId) {
+        return findUser(userId).orElseGet(() -> createUser(userId).get());
     }
 
     @Cacheable(value = "userCache", key = "#userId")
-    public Optional<User> getUser(String userId) {
+    public Optional<User> findUser(String userId) throws HttpClientErrorException {
         try {
             return Optional.of(userApiTemplate.getForObject("/v1/users/{userId}",
                     User.class,
                     userId));
-        } catch (RestClientException ex) {
+        } catch (HttpClientErrorException ex) {
             logger.debug("Unable to find User with id of {}", userId);
+            if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
+                return Optional.empty();
+            }
+            throw ex;
         }
-        return Optional.empty();
     }
 
     @CacheEvict(value = "userCache", key = "#userId")
-    public Optional<User> createUser(String userId) {
+    public Optional<User> createUser(String userId) throws HttpClientErrorException {
         try {
             return Optional.of(userApiTemplate.postForObject("/v1/users/",
                     createUserRequest(userId),
                     User.class));
-        } catch (RestClientException ex) {
+        } catch (HttpClientErrorException ex) {
             logger.debug("Unable to create User with id of {}, ex {}", userId, ex);
-            return getUser(userId);
+            if (ex.getStatusCode() == HttpStatus.CONFLICT) {
+                return findUser(userId);
+            }
+            throw ex;
         }
 
     }
